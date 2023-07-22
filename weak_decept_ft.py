f1c13510b077c50c0d9120a8bc0ec2a8eb4e39b8
Alex Mallen <atmallen8@gmail.com>
2023-07-22 02:14:10 +0000

    add KL losses

--- weak_decept_ft.py
+++ weak_decept_ft.py
@@ -19,6 +19,8 @@ wandb.login()
 parser = ArgumentParser()
 parser.add_argument("--model-name", type=str, default="EleutherAI/pythia-160m")
 parser.add_argument("--ds-name", type=str, default="./custom-datasets/popqa_90")
+parser.add_argument("--objective", type=str, default="standard", choices=["standard", "KL", "KL+standard"])
+parser.add_argument("--kl-weight", type=float, default=0.1)
 parser.add_argument("--max-length", type=int, default=1024)
 parser.add_argument("--lr", type=float, default=2e-5)
 parser.add_argument("--n-epochs", type=int, default=2)
@@ -33,6 +35,7 @@ parser.add_argument("--lora-rank", type=int, default=32)
 parser.add_argument("--lora-alpha", type=int, default=32)
 parser.add_argument("--lora-dropout", type=float, default=0.1)
 parser.add_argument("--device", type=str, default="cuda")
+parser.add_argument("--device2", type=str, default="cuda")
 parser.add_argument("--no-peft", action="store_true")
 parser.add_argument("--target-modules", nargs="+", default=["dense_h_to_4h", "dense_4h_to_h", "query_key_value"])
 
@@ -67,6 +70,7 @@ cfg = vars(args)
 ds = load_from_disk(ds_name)
 
 ds["train"] = ds["train"].shuffle()
+ds["validation"] = ds["validation"].shuffle()
 ds["test"] = ds["test"].shuffle()
 
 n_train = len(ds["train"]) if n_train == -1 else n_train
@@ -87,17 +91,52 @@ if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
 
 
+def to_tensors(seq, batch_size):
+    out = []
+    for i in range(batch_size):
+        out.append(torch.tensor(seq[i][:max_length]))
+    return out
+
+
+def encode_choices(examples):
+    return [tokenizer.encode(cs, add_special_tokens=False, return_tensors="pt").squeeze()
+             for cs in examples["choices"]]
+
+
+def pad(seq, with_tok, batch_size, max_length):
+    # in-place pad everything to max_length and convert to tensors
+    for i in range(batch_size):
+        seq[i] = [with_tok] * (max_length - len(seq[i])) + seq[i]
+
+
+def tokenize_eval_examples(examples):
+    # similar to tokenize_examples, but without the label
+    batch_size = len(examples["text"])
+
+    # tokenize inputs
+    model_inputs = tokenizer(examples["text"])
+    
+    pad(model_inputs["input_ids"], tokenizer.pad_token_id, batch_size, max_length)
+    pad(model_inputs["attention_mask"], 0, batch_size, max_length)
+
+    out_dict = model_inputs
+    out_dict["labels"] = torch.tensor(examples["label"])
+    out_dict["true_labels"] = torch.tensor(examples["true_label"])
+    out_dict["choice_ids"] = encode_choices(examples)
+    out_dict["p_true"] = torch.tensor(examples["label"], dtype=torch.float32)
+    return out_dict
+
+
 # define templatize and tokenize functions
 def tokenize_examples(examples):
     batch_size = len(examples["text"])
     print(batch_size)
 
-    # apply template to each example
-    texts = examples["text"]
-    targets = [choices[label] for label, choices in zip(examples["label"], examples["choices"])]
+    # label could be a float, representing the probability the model should assign to the statement
+    targets = [choices[int(label)] for label, choices in zip(examples["label"], examples["choices"])]
     
     # tokenize inputs and targets
-    inputs = tokenizer(texts)
+    inputs = tokenizer(examples["text"])
     labels = tokenizer(targets)
 
     # concatenate inputs and labels
@@ -113,55 +152,17 @@ def tokenize_examples(examples):
         inputs["attention_mask"][i] = [1] * len(inputs["input_ids"][i])
     print(max([len(input_ids) for input_ids in inputs["input_ids"]]))
 
-    # pad everything to max_length and convert to tensors
-    for i in range(batch_size):
-        sample_input_ids = inputs["input_ids"][i]
-        label_input_ids = labels["input_ids"][i]
-        inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
-            max_length - len(sample_input_ids)
-        ) + sample_input_ids
-        inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + inputs[
-            "attention_mask"
-        ][i]
-        labels["input_ids"][i] = [-100] * (max_length - len(sample_input_ids)) + label_input_ids
-        inputs["input_ids"][i] = torch.tensor(inputs["input_ids"][i][:max_length])
-        inputs["attention_mask"][i] = torch.tensor(inputs["attention_mask"][i][:max_length])
-        labels["input_ids"][i] = torch.tensor(labels["input_ids"][i][:max_length])
-        
-    inputs["labels"] = labels["input_ids"]
-    inputs["choice_ids"] = [tokenizer.encode(cs, add_special_tokens=False, return_tensors="pt").squeeze() for cs in examples["choices"]]
+    pad(inputs["input_ids"], tokenizer.pad_token_id, batch_size, max_length)
+    pad(inputs["attention_mask"], 0, batch_size, max_length)
+    pad(labels["input_ids"], -100, batch_size, max_length)
+    
+    inputs["input_ids"] = to_tensors(inputs["input_ids"], batch_size)
+    inputs["attention_mask"] = to_tensors(inputs["attention_mask"], batch_size)
+    inputs["labels"] = to_tensors(labels["input_ids"], batch_size)
+    inputs["choice_ids"] = encode_choices(examples)
+    inputs["p_true"] = torch.tensor(examples["label"], dtype=torch.float32)
     print(tokenizer.decode(inputs["input_ids"][0]))
     return inputs
-
-
-def tokenize_eval_examples(examples):
-    # similar to tokenize_examples, but without the label
-
-    batch_size = len(examples["text"])
-
-    # apply template to each example
-    texts = examples["text"]
-
-    # tokenize inputs
-    model_inputs = tokenizer(texts)
-    
-    # pad everything to max_length and convert to tensors
-    for i in range(batch_size):
-        sample_input_ids = model_inputs["input_ids"][i]
-        model_inputs["input_ids"][i] = [tokenizer.pad_token_id] * (
-            max_length - len(sample_input_ids)
-        ) + sample_input_ids
-        model_inputs["attention_mask"][i] = [0] * (max_length - len(sample_input_ids)) + model_inputs[
-            "attention_mask"
-        ][i]
-        model_inputs["input_ids"][i] = torch.tensor(model_inputs["input_ids"][i][:max_length])
-        model_inputs["attention_mask"][i] = torch.tensor(model_inputs["attention_mask"][i][:max_length])
-    
-    out_dict = model_inputs
-    out_dict["labels"] = torch.tensor(examples["label"])
-    out_dict["true_labels"] = torch.tensor(examples["true_label"])
-    out_dict["choice_ids"] = [tokenizer.encode(cs, add_special_tokens=False, return_tensors="pt").squeeze() for cs in examples["choices"]]
-    return out_dict
 
 
 # templateize and tokenize train
@@ -219,6 +220,8 @@ if use_peft:
     model = get_peft_model(model, peft_config)
     model.print_trainable_parameters()
 model = model.to(device)  # we want to keep the lora params in single precision, so don't call half() after pefting
+if "KL" in args.objective:
+    base_model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16).to(args.device2).eval()
 
 num_erroneous = 0
 for row in ds["validation"]:
@@ -282,6 +285,8 @@ def eval_model(use_tqdm=False, dataloader=eval_dataloader):
 
 acc, acc_err, acc_non_err = eval_model(use_tqdm=False)
 print(f"Initial Acc: {acc}, Acc on erroneous: {acc_err}, Acc on non-erroneous: {acc_non_err}")
+train_acc, train_acc_err, train_acc_non_err = eval_model(use_tqdm=False, dataloader=train_eval_dataloader)
+print(f"Initial Train Acc: {train_acc}, Train Acc on erroneous: {train_acc_err}, Train Acc on non-erroneous: {train_acc_non_err}")
 
 # only the LORA parameters should be updated
 learnable_parameters = [p for p in model.parameters() if p.requires_grad]
@@ -294,15 +299,62 @@ lr_scheduler = get_linear_schedule_with_warmup(
         num_training_steps=(len(train_dataloader) * num_epochs),
     )
 
+
+def redistribute_ps(ps, choice_ids, p_true):
+    choice_ids, p_true = choice_ids.to(ps.device), p_true.to(ps.device)
+    # redistribute the total probability mass of the two choices in
+    # the base model according to the true probability
+    combined_base_ps = torch.gather(ps, 1, choice_ids).sum(dim=-1)  # shape: (batch_size)
+    redistributed_base_ps = torch.stack(
+        [combined_base_ps * (1 - p_true), combined_base_ps * p_true],
+        dim=-1,
+    ).type_as(ps)  # shape: (batch_size, 2)
+    # scatter is the setter version of gather
+    ps.scatter_(1, choice_ids, redistributed_base_ps)  # shape: (batch_size, vocab_size)
+    return ps
+
+def KL(ps, base_ps):
+    """Compute the KL divergence between the model logits and the base model logits
+     logits: (batch_size, vocab_size) last token logits
+     base_logits: (batch_size, vocab_size) last token logits
+     choice_ids: (batch_size, 2) ids of the two choices
+     p_true: (batch_size) probability of the true choice
+    """
+    base_ps = base_ps.detach().to(ps.device)
+
+    ps = ps.clamp(1e-15, 1 - 1e-4)  # avoid numerical issues
+    base_ps = base_ps.clamp(1e-15, 1 - 1e-4)
+    # compute KL divergence
+    kl = (ps * (ps.log() - base_ps.log())).sum(dim=-1)  # shape: (batch_size)
+    return kl.mean()
+
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
     for step, batch in enumerate(tqdm(train_dataloader)):
         choice_ids = batch.pop("choice_ids")
+        p_true = batch.pop("p_true")
         batch = {k: v.to(device) for k, v in batch.items()}
 
         outputs = model(**batch)
-        loss = outputs.loss
+        if "KL" in args.objective:
+            device2_batch = {k: v.to(args.device2) for k, v in batch.items()}
+            with torch.no_grad():
+                base_outputs = base_model(**device2_batch)
+            
+            ps = outputs.logits[:, -1, :].type(torch.float64).softmax(dim=-1)
+            base_ps = base_outputs.logits[:, -1, :].type(torch.float64).softmax(dim=-1)
+            if args.objective == "KL":
+                loss = KL(ps, redistribute_ps(base_ps, choice_ids, p_true))
+            elif args.objective == "KL+standard":
+                kl = KL(ps, base_ps)
+                if step % 50 == 0:
+                    print("KL:", kl, "Standard:", outputs.loss, "Ratio:", kl / outputs.loss)
+                loss = args.kl_weight * kl + outputs.loss
+        elif args.objective == "standard":
+            loss = outputs.loss
+        else:
+            raise ValueError(f"Unknown objective: {args.objective}")
 
         total_loss += loss.detach().float()
         loss.backward()
@@ -321,7 +373,7 @@ for epoch in range(num_epochs):
             model.train()
             
     print("Epoch {} loss: {}".format(epoch, total_loss / len(train_dataloader)))
-    
+
 wandb.finish()
 
 # save model
